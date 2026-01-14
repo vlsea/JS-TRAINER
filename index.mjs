b0946d87606d97076c9b1482d63d038e43454e0b
Егор <vlsea@mail.ru>
2026-01-14 13:26:59 +1000

    MSHP2

--- index.mjs
+++ index.mjs
@@ -3,4 +3,20 @@ import "./index.mjs";
 document.getElementById("app").innerHTML = `
 <h1>Hello JavaScript!</h1>
 `;
-console.log (2+2)
+const monthOfBerth = 11;
+console.log(monthOfBerth);
+console.log("добро" + " пожаловать" + " на курс" + " Егор");
+// alert("14.01.2026");
+
+let one = document.getElementById("one");
+one.textContent = "Hello, World";
+
+// Number 1 2 847482287
+// string "My name is Deni"
+// Bigint 987777777777777777777n
+// Boolean true false
+// Null
+// undefined
+// symbol
+// Object
+
