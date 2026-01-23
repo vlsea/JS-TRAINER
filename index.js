b5fde2193c989ac512025f764d5781565bef0898
Егор <vlsea@mail.ru>
2026-01-23 15:13:14 +1000

    MSHP4

---
+++ index.js
@@ -0,0 +1,24 @@
+// import "./index.mjs";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Egorushka!</h1>
+`;
+
+
+// let a = 10
+// let b = 5
+// if (a + b >= 15) {
+//     console.log ("да")
+// }
+// else {
+//     console.log("нет")
+// }
+
+
+let x = 5
+let y = "5"
+if (x === y) {
+    console.log("равны")
+}
+    else {console.log ("не равны")
+}
