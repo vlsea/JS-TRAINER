45555089c3407bea2009825661aa76ae62edc183
Егор <vlsea@mail.ru>
2026-02-04 12:40:18 +1000

    MSHP6

--- index.js
+++ index.js
@@ -4,21 +4,52 @@ document.getElementById("app").innerHTML = `
 <h1>Hello Egorushka!</h1>
 `;
 
-// let summ = 0;
-// for (let i = 0; summ <= 50; i++) {
-//     console.log(summ);summ ++
+
+
+// let a = null;
+// let b = 1;
+// let c = 2;
+// console.log(a ?? b ?? c);
+
+
+
+// let a = 2;
+// let userInput = prompt("введите число");
+// let numberInput = +userInput;
+// switch (numberInput) {
+//   case a:
+//         alert("верно");
+//         break
+//     default:
+//   alert("не верно");
+//     break
 // }
 
 
+// let userInput = prompt("Введите месяц (1-12):");
+// let month = +userInput;
 
-// for (let i = 50; i >= 25; i--){
-//     console.log(i)
+// switch (month) {
+//   case 1:
+//   case 2:
+//   case 12:
+//     console.log("Зима");
+//     break;
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log("Весна");
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     console.log("Лето");
+//     break;
+//   case 9:
+//   case 10:
+//   case 11:
+//     console.log("Осень");
+//     break;
+//   default:
+//     console.log("Неверный месяц (1-12)");
 // }
-
-
-// for (let i = 2500; i <= 3000; i++){
-//     if (i%2===1) {console.log(i)}
-// }
-
-
-
