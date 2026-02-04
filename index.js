152a7b146046d9177d3e4d968e8a9148e63c6379
Егор <vlsea@mail.ru>
2026-02-04 12:40:18 +1000

    MSHP6-switch case

--- index.js
+++ index.js
@@ -5,20 +5,51 @@ document.getElementById("app").innerHTML = `
 `;
 
 
-// let a = 10
-// let b = 5
-// if (a + b >= 15) {
-//     console.log ("да")
-// }
-// else {
-//     console.log("нет")
+
+// let a = null;
+// let b = 1;
+// let c = 2;
+// console.log(a ?? b ?? c);
+
+
+
+// let a = 2;
+// let userInput = prompt("введите число");
+// let numberInput = +userInput;
+// switch (numberInput) {
+//   case a:
+//         alert("верно");
+//         break
+//     default:
+//   alert("не верно");
+//     break
 // }
 
 
-let x = 5
-let y = "5"
-if (x === y) {
-    console.log("равны")
-}
-    else {console.log ("не равны")
-}
+// let userInput = prompt("Введите месяц (1-12):");
+// let month = +userInput;
+
+// switch (month) {
+//   case 1:
+//   case 2:
+//   case 12:
+//     console.log("Зима");
+//     break;
+//   case 3:
+//   case 4:
+//   case 5:
+//     console.log("Весна");
+//     break;
+//   case 6:
+//   case 7:
+//   case 8:
+//     console.log("Лето");
+//     break;
+//   case 9:
+//   case 10:
+//   case 11:
+//     console.log("Осень");
+//     break;
+//   default:
+//     console.log("Неверный месяц (1-12)");
+// }
