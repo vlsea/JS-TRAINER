4136d0b168d722b83615624930d64023f9720c13
Егор <vlsea@mail.ru>
2026-02-18 12:38:33 +1000

    MSHP9

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-function changeSize() {
-  const element = document.querySelector(".element");
-  element.style.width = "300px";
-  element.style.height = "200px";
-  element.style.backgroundColor = "#4CAF50";
-  element.style.padding = "20px";
-  element.style.borderRadius = "10px";
+
+
+document.getElementById("button_1").onclick = function () {
+  let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  console.log(array[5])
 }
 
-document.getElementById("btn1").addEventListener("click", changeSize);
 
-function addClasses() {
-  const element = document.getElementById("myElement");
-  element.classList.add("big", "red", "rounded");
+document.getElementById("button_2").onclick = function () {
+  let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  for (let i = 0; i < array.length; i++) {
+    if (i % 2 === 0)
+      console.log(i)
+  }
+}
+document.getElementById("button_3").onclick = function () {
+  let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  let evenNumbers = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (i % 2 === 1) console.log(i);
+  }
 }
 
-document.getElementById("btn2").addEventListener("click", addClasses);
-
-function removeClasses() {
-  const element = document.getElementById("myElement");
-  element.classList.remove("big", "red", "rounded");
+document.getElementById("button_4").onclick = function () {
+  let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  console.log(array.reverse());
 }
 
-document.getElementById("btn3").addEventListener("click", removeClasses);
+
