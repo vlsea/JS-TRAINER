be1379d141d00bf89f16a1e63b642d8fd95edf52
Егор <vlsea@mail.ru>
2026-06-19 13:04:26 +1000

    React#2Task

--- my/src/App.js
+++ my/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Счетчик</h1>
+      <h2>{count}</h2>
+
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(0)} style={{ margin: "0 10px" }}>
+        Сброс
+      </button>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 }
